4977a3ff8e13e422f802707fad4dac68b9cad060
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-25 15:42:08 +0000

    added tables

--- db/migrate/20171125153422_create_students.rb
+++ db/migrate/20171125153422_create_students.rb
@@ -1,4 +1,10 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :preferred_name
+      t.integer :teacher_id
+    end
   end
 end
--- db/migrate/20171125153428_create_feedbacks.rb
+++ db/migrate/20171125153428_create_feedbacks.rb
@@ -1,4 +1,9 @@
 class CreateFeedbacks < ActiveRecord::Migration[5.1]
   def change
+    create_table :feedbacks do |t|
+      t.string :content
+      t.integer :student_id
+      t.timestamps
+    end
   end
 end
