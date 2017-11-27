ad4c8caa9275e876ca7ac1d546b1f202d24e4cf2
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-27 18:02:34 +0000

    adding feedback options, like new and index

---
+++ app/controllers/feedbacks_controller.rb
@@ -0,0 +1,19 @@
+require 'rake-flash'
+class FeedbacksController < ApplicationController
+ use Rake::Flash
+
+
+ get '/feedbacks/new' do
+   erb :"/feedbacks/new"
+ end
+
+ post '/feedbacks/new' do
+   @feedback = Feedback.create(content: params[:content])
+   @student = Student.find(session[:user_id])
+   @student.feedbacks << @feedbacks
+   @feedback.save
+   @student.save
+   redirect to '/feedbacks/index'
+ end
+
+end
