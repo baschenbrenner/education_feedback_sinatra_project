76ea407b34e7a2efc80905017d84b1b34fc14720
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-27 08:04:06 +0000

    added some students to table to be able to see them in app

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+require_relative '../config/environment.rb'
+
+Student.create(first_name: "Amanda", last_name: "Yates", preferred_name: "Amanda", password: "amandayates")
+Student.create(first_name: "Franklin", last_name: "Smith", preferred_name: "Frank", password: "franksmith")
+Student.create(first_name: "Ida", last_name: "Wells", preferred_name: "Bessie", password:"idawells")
+Student.create(first_name: "Keith", last_name: "Richards", preferred_name: "Dickie", password: "keithrichards")
+Student.create(first_name: "Bruce", last_name: "Hornsby", preferred_name: "Bruce", password:"brucehornsby")
