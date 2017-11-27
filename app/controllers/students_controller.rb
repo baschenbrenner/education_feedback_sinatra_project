b2291fae64556fe594da38426cb4e8b02b32a6e8
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-27 09:10:11 +0000

    added new student features, like login, signup

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,10 +16,24 @@ class StudentsController < ApplicationController
     @student=Student.find_by(first_name: params[:first_name], last_name: params[:last_name])
     if @student && @student.authenticate(params[:password])
       session[:user_id]=@student.id
-    redirect to "/teachers/show/#{@student.id}"
+    redirect to "/students/show/#{@student.id}"
     else
       flash[:message]="You entered invalid login credentials."
-      redirect '/teachers/login'
+      redirect '/students/login'
     end
   end
+
+
+  post '/students/signup' do
+    @student=Student.create(first_name: params[:first_name], last_name: params[:last_name], preferred_name: params[:preferred_name], password: params[:password])
+    @teacher = Teacher.find(params[:teacher])
+    @student.teacher=@teacher
+    @student.save
+    flash[:message]="You have successfully created an account. Now you can log in!"
+    redirect to '/students/login'
+  end
+
+  get 'students/show/:id'
+    @student= Student.find(params[:id])
+    erb :"students/show"
 end
