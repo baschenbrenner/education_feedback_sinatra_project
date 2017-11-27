48149c0d902d305df4c366decdd4f6bf24a3633d
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-27 08:59:44 +0000

    added some new student pages

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,23 @@ class StudentsController < ApplicationController
   get "/students" do
     erb :"students/index"
   end
+
+  get "/students/login" do
+    erb :"students/login"
+  end
+
+  get "/students/signup" do
+    erb :"students/signup"
+  end
+
+  post "/students/login" do
+    @student=Student.find_by(first_name: params[:first_name], last_name: params[:last_name])
+    if @student && @student.authenticate(params[:password])
+      session[:user_id]=@student.id
+    redirect to "/teachers/show/#{@student.id}"
+    else
+      flash[:message]="You entered invalid login credentials."
+      redirect '/teachers/login'
+    end
+  end
 end
--- config.ru
+++ config.ru
@@ -6,7 +6,7 @@ end
 
 use Rack::MethodOverride
 
-#use StudentsController
+use StudentsController
 use TeachersController
 #use FeedbacksController
 run ApplicationController
