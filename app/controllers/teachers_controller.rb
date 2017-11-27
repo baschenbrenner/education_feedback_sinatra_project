dee875e0d4c0cdd1f89fdbdfe065993ef52443ae
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-27 01:37:07 +0000

    Added login and sign up page for teachers

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -10,4 +10,16 @@ class TeachersController < ApplicationController
   get '/teachers/signup' do
     erb :"teachers/signup"
   end
+
+  post '/teachers/signup' do
+    @teacher=Teacher.create(first_name: params[:first_name], last_name: params[:last_name], preferred_name: params[:preferred_name], password: params[:password])
+    @teacher.save
+    redirect to '/teachers/login'
+  end
+
+  post '/teachers/signup' do
+    @teacher=Teacher.find_by(first_name: params[:first_name], last_name: params[:last_name])
+    if @teacher && @teacher.authenticate(params[:password])
+    redirect to '/teachers/show'
+  end
 end
